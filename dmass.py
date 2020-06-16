a0ca46ac348994bdcee3661f547c325873a95182
adammoutik <63082280+adammoutik@users.noreply.github.com>
2020-06-17 01:46:10 +0200

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("Njk1NDQxNjIxNTkyMDQ3NzE3.XoaOsQ.hYSWVsNFYXPqCGNoLCMfg7U2v9Y")                
+client.run("token")                
